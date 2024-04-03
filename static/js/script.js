1588b9b435c69ab118f1256eedabf6dc4f9a3c2c
weijielyu <wlyu3@ucmerced.edu>
2024-04-03 14:58:48 -0700

    tmp update

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 // For RGB / global segmentation results
 var video_width = 779;
 const VIDEO_ASPECT_RATIO = 779 / 518;
-var video_names = ["room", "room_edit"];
+var display_level = 0;
+var video_names = ["teaser"];
     
 var videos = [];
 var current_video_idx = 0;
@@ -14,35 +15,8 @@ function load_videos() {
 
 window.onload = function() {
   resize_canvas();
-  const root = videos[1]
-  const checkbox = document.getElementById('opacity-toggle')
-
-  load_videos();
-  checkbox.addEventListener('change', (event) => {
-    if (event.currentTarget.checked) {
-      // root.style.setProperty("--opacity", `100%`);
-      current_video_idx = 0;
-    } else {
-      // root.style.setProperty("--opacity", `0%`);
-      current_video_idx = 1;
-    }
-  })
-
-  // const checkbox = document.getElementById('opacity-toggle')
-
-  // load_videos();
-  // checkbox.addEventListener('change', (event) => {
-  //   if (event.currentTarget.checked) {
-  //     current_video_idx = 0;
-  //     videos[1].style.setProperty("--opacity", `100%`);
-  //   } else {
-  //     current_video_idx = 1;
-  //     videos[1].style.setProperty("--opacity", `0%`);
-  //   }
-  // })
   load_videos();
   videos[0].play();
-  videos[1].play();
 }
 
 /* Synchronize main_results, and its canvas(es) to have the same size. */
@@ -138,7 +112,7 @@ $(() => {
         );  // RGB
       ctx.drawImage(
         video,
-        779+newLeftVideo, 0, 779-newLeftVideo, 518, newLeft, 
+        779*(display_level+1)+newLeftVideo, 0, 779-newLeftVideo, 518, newLeft, 
         0, video_width-newLeft, video_width/VIDEO_ASPECT_RATIO
         ); // Segmentation
     });
@@ -173,6 +147,108 @@ $(() => {
     });
 }, 1000 / 60);  // 30fps
 
+/* Switcher. */
+// Contains logic for switching between coarse/medium/fine.
+$(() => {
+  $(".switcher").each((switcher_index, switcher) => {
+    const $switcher = $(switcher);
+
+    const $inputContainer = $("<div>", { class: "switcher-labels" });
+
+    let $current = null;
+
+    $switcher.children().each((switcher_child_index, child) => {
+      const $child = $(child);
+
+      const linkId =
+        "switcher-group-" +
+        switcher_index.toString() +
+        "-" +
+        switcher_child_index.toString();
+      const $input = $("<input>", {
+        type: "radio",
+        name: "switcher-group-" + switcher_index.toString(),
+        id: linkId,
+        checked: switcher_child_index === 0,
+        click: function () {
+          // Your onclick event logic goes here
+          $current.addClass("switcher-hidden");
+          display_level = switcher_child_index;
+
+          $current = $([]);
+          $.merge($current, $child);
+          $.merge($current, $input);
+          $.merge($current, $label);
+
+          $current.removeClass("switcher-hidden");
+        },
+      });
+      const $label = $("<label>", {
+        text: $child.data("switcher-label"),
+        for: linkId,
+      });
+      $inputContainer.append($("<div>").append($input).append($label));
+
+      if (switcher_child_index !== 0) {
+        $child.addClass("switcher-hidden");
+        $input.addClass("switcher-hidden");
+        $label.addClass("switcher-hidden");
+      } else {
+        $current = $([]);
+        $.merge($current, $child);
+        $.merge($current, $input);
+        $.merge($current, $label);
+      }
+    });
+
+    const $label = $("<label>", {
+      text: $switcher.data("switcher-title") + ":",
+    });
+    $inputContainer.prepend($label);
+
+    $switcher.append($inputContainer);
+  });
+});
+
+/* Switcher. */
+// Contains logic for switching between coarse/medium/fine.
+$(() => {
+  $(".results-slide-row").each((switcher_index, switcher) => {
+    const $switcher = $(switcher);
+    console.log($switcher);
+    console.log($switcher.children());
+
+    $switcher.children().each((switcher_child_index, child) => {
+      const $child = $(child);
+
+      const $input = $("<button>", {
+        class: "thumbnail-btn",
+        id: "thumb-" + switcher_index.toString(),
+        click: function () {
+          // Your onclick event logic goes here
+          current_video_idx = switcher_child_index;
+          current_video = videos[current_video_idx]
+          current_video.currentTime = 0;
+          current_video.play();
+          set_play_pause_icon();
+        },
+      });
+      const $img = $("<img>", {
+        class: "thumbnails",
+        alt: "paper",
+        src: $child.data("img-src"),
+      });
+      $input.append($img);
+      const $label = $("<label>", {
+        text: $child.data("label"),
+        class: "thumbnail_label",
+      });
+      $input.append($label);
+      $switcher.append($input);
+    });
+  });
+});
+
 function set_play_pause_icon() {
   button = document.getElementById('play-btn')
   current_video = videos[current_video_idx]
@@ -193,4 +269,68 @@ function play_pause() {
     current_video.pause();
   }
   set_play_pause_icon();
+}
+
+function fullscreen() {
+  current_video = videos[current_video_idx]
+  current_video.style.visibility = "visible";
+  const fullscreenElement =
+    document.fullscreenElement ||
+    document.mozFullScreenElement ||
+    document.webkitFullscreenElement ||
+    document.msFullscreenElement;
+  if (fullscreenElement) {
+    exitFullscreen();
+  } else {
+    launchIntoFullscreen(current_video);
+  }
+}
+
+function download() {
+  current_video = videos[current_video_idx]
+  var link = document.createElement('a');
+  link.download = video_names[current_video_idx] + '.mp4';
+  link.href = download_paths[current_video_idx];
+  link.click();
+}
+
+function launchIntoFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else {
+    element.classList.toggle('fullscreen');
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+if (document.addEventListener)
+{
+ document.addEventListener('fullscreenchange', exitHandler, false);
+ document.addEventListener('mozfullscreenchange', exitHandler, false);
+ document.addEventListener('MSFullscreenChange', exitHandler, false);
+ document.addEventListener('webkitfullscreenchange', exitHandler, false);
+}
+
+function exitHandler()
+{
+ if (!document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement)
+ {
+  current_video = videos[current_video_idx]
+  current_video.style.visibility = "hidden";
+ }
 }
